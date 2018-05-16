a3c2be8f0d3adaeff1b4f424673b9234ccbbffc3
jrambold <jrambold@gmail.com>
2018-05-16 10:30:04 -0600

    station info displays

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,6 +2,5 @@ class SearchController < ApplicationController
   def index
     search = Search.new(params['q'])
     @stations = search.nearby(['ELEC', 'LPG'], 6, 10)
-    require 'pry'; binding.pry
   end
 end
--- app/models/search.rb
+++ app/models/search.rb
@@ -4,8 +4,13 @@ class Search
     @zip = zip
   end
 
+  # (name, address, fuel_types, distance, access_times)
   def nearby(fuel_types, distance, limit)
-    @search_service.get_url("/api/alt-fuel-stations/v1/nearest.json?location=#{@zip}&fuel_type=#{fuel_types[0]},#{fuel_types[1]}&limit=#{limit}&radius=#{distance}&api_key=#{ENV['NREL_API_KEY']}")
+    raw_stations = @search_service.get_url("/api/alt-fuel-stations/v1/nearest.json?location=#{@zip}&fuel_type=#{fuel_types[0]},#{fuel_types[1]}&limit=#{limit}&radius=#{distance}&api_key=#{ENV['NREL_API_KEY']}")
+    stations = raw_stations[:fuel_stations].map do |station|
+      Station.new(station[:station_name],station[:street_address],station[:fuel_type_code],station[:distance],station[:access_days_time])
+    end
+    stations
   end
 
 end
